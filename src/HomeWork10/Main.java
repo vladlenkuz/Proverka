38fedea752e4ec1a1948b351504a5a72b6963bc6
vladlenkuz <81929951+vladlenkuz@users.noreply.github.com>
2021-05-13 16:04:05 +0300

    Java 2 lesson 2

---
+++ src/HomeWork10/Main.java
@@ -0,0 +1,52 @@
+package HomeWork10;
+
+import java.lang.reflect.Array;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[][] array1 = new String[][]{{"1", "2", "3", "4"},
+                {"2", "2", "2", "3"},
+                {"1", "2", "2", "2"},
+                {"2", "2", "2", "@"}};
+
+        try {
+            try {
+                int result = method(array1);
+                System.out.println(result);
+            } catch (MyArraySizeException e) {
+                System.out.println("Размер массива превышен!");
+            }
+        }
+        catch (MyArrayDataException e) {
+            System.out.println("Неправильное значение массива!");
+            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+        }
+
+    }
+
+
+    public static int method(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int count = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    count = count + Integer.parseInt(arr[i][j]);
+                }
+                catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+
+        }
+        return count;
+    }
+
+}
+
